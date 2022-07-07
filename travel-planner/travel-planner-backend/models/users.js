67eaaa1d2a61a8e485319e0f213cbbf94de6825a
Kimberly Truong <kimberlytruong@fb.com>
2022-07-07 13:32:23 -0700

    Move users functions to users.models.

--- travel-planner/travel-planner-backend/models/users.js
+++ travel-planner/travel-planner-backend/models/users.js
@@ -1,4 +1,20 @@
+const Parse = require("parse/node")
+
+const APP_ID = "yS7mzf9F0VFtrumR3wVbkY6wNTM3zFbMf4H7s4HP";
+const JS_KEY = "lyK93mhzenGxQh58QPrx9vvXoJ1FLk9AMmGaCJHL";
+const MASTER_KEY = "Sh5jO61tl94TLrWLPm8RBvk1T7l3fHOBLJXTv4LT"
+
+// Initialize Parse
+Parse.initialize(`${APP_ID}`, `${JS_KEY}`, `${MASTER_KEY}`);
+Parse.serverURL = "https://parseapi.back4app.com/parse"
+
 class Users {
+    static async logIn(username, password) {
+        let user = await Parse.User.logIn(username, password)
+
+        return ({sessionToken: await user.getSessionToken()})
+    }
+
     static async logOut(sessionToken) {
         let query = new Parse.Query("_Session")
 
@@ -23,4 +39,17 @@ class Users {
             }
         })
     }
+
+    static async signUp(username, email, password) {
+        let user = new Parse.User()
+
+        user.set("username", username)
+        user.set("email", email)
+        user.set("password", password)
+
+        let newUser = await user.signUp()
+        await this.logOut(newUser.getSessionToken())
+    }
 }
+
+module.exports = Users
--- travel-planner/travel-planner-backend/routes/users.js
+++ travel-planner/travel-planner-backend/routes/users.js
@@ -1,56 +1,15 @@
-const Parse = require("parse/node")
 const express = require("express")
 const router = express.Router()
 const bodyParser = require('body-parser')
+const userModel = require('./../models/users')
 
 router.use(bodyParser.json())
 
-const APP_ID = "yS7mzf9F0VFtrumR3wVbkY6wNTM3zFbMf4H7s4HP";
-const JS_KEY = "lyK93mhzenGxQh58QPrx9vvXoJ1FLk9AMmGaCJHL";
-const MASTER_KEY = "Sh5jO61tl94TLrWLPm8RBvk1T7l3fHOBLJXTv4LT"
-
-// Initialize Parse
-Parse.initialize(`${APP_ID}`, `${JS_KEY}`, `${MASTER_KEY}`);
-Parse.serverURL = "https://parseapi.back4app.com/parse"
-
-const logOut = async (sessionToken) => {
-    let query = new Parse.Query("_Session")
-
-    /* Get all sessions with the session token (there should only be one) */
-    query.equalTo("sessionToken", sessionToken)
-
-    query.first({useMasterKey:true}).then(function (user) {
-        if (user) {
-            console.log(user)
-
-            /* Remove the session from the table. */
-            user.destroy({useMasterKey:true}).then(function (res) {
-                console.log("Successfully destroyed session!")
-            }).catch(function (error) {
-                console.log(error)
-                return null
-            })
-        }
-        else {
-            console.log("Nothing found? ...")
-            return null
-        }
-    })
-}
-
 router.post('/register', async (req, res) => {
     let infoUser = req.body
-    let user = new Parse.User()
-
-    user.set("username", infoUser.username)
-    user.set("email", infoUser.email)
-    user.set("password", infoUser.password)
 
     try {
-        let newUser = await user.signUp()
-
-        await logOut(newUser.getSessionToken()) // NEED TO CONFIRM THIS WORKS
-
+        userModel.signUp(infoUser.username, infoUser.email, infoUser.password)
 
         res.status(200).send({ loginMessage: '', registerMessage: `Account created! Username: ${infoUser.username} Please check ${infoUser.email} to verify.`, typeStatus: "success", infoUser: infoUser })
     } catch (error) {
@@ -62,9 +21,9 @@ router.post('/login', async (req, res) => {
     let infoUser = req.body
 
     try {
-        let user = await Parse.User.logIn(infoUser.username, infoUser.password)
+        let sessionToken = (await userModel.logIn(infoUser.username, infoUser.password)).sessionToken
 
-        res.status(200).send({ loginMessage: `${infoUser.username} has been logged in!`, registerMessage: '', typeStatus: "success", infoUser: infoUser, sessionToken: await user.getSessionToken() })
+        res.status(200).send({ loginMessage: `${infoUser.username} has been logged in!`, registerMessage: '', typeStatus: "success", infoUser: infoUser, sessionToken: sessionToken })
     } catch (error) {
         res.status(409).send({ loginMessage: error.message, registerMessage: '', typeStatus: "danger", infoUser: infoUser })
     }
@@ -72,7 +31,7 @@ router.post('/login', async (req, res) => {
 
 router.post('/logout', async (req, res) => {
     let sessionToken = req.body.sessionToken
-    await logOut(sessionToken)
+    await userModel.logOut(sessionToken)
 })
 
 module.exports = router
