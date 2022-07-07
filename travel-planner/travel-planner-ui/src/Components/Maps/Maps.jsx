4c7a82c92d4609994983057b69c7bd6a90e879cf
Kimberly Truong <kimberlytruong@fb.com>
2022-07-07 15:48:24 -0700

    Added embedded map to /planner page.

--- travel-planner/travel-planner-ui/src/Components/Maps/Maps.jsx
+++ travel-planner/travel-planner-ui/src/Components/Maps/Maps.jsx
@@ -1,9 +1,20 @@
-import "./Maps.css"
+import "./Maps.css";
 
 export default function Maps() {
-    return (
-        <div className="maps-container">
-            
-        </div>
-    )
+    /* This will need to be moved to the backend for security reasons!! */
+    // for more map modes, see: https://developers.google.com/maps/documentation/embed/embedding-map#choosing_map_modes
+    const MAP_MODE = "place" // directions is also an option!!
+    const API_KEY = "AIzaSyDUuAbmaWWY2Lk6iKlktVEPRAIrTI0__eg"
+    const PARAMETERS = "q=City+Hall,New+York,NY" //&center=latitude,logitude
+
+	return (
+		<div className="maps-container">
+			<iframe
+				frameBorder="0"
+				referrerPolicy="no-referrer-when-downgrade"
+                src={`https://www.google.com/maps/embed/v1/${MAP_MODE}?key=${API_KEY}&${PARAMETERS}`}
+                className="embedded-map"
+			></iframe>
+		</div>
+	);
 }
