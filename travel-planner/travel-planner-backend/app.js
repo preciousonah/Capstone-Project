8c85c76216febfa98eb3ae1ee63f27fc8cbc7336
Kimberly Truong <kimberlytruong@fb.com>
2022-07-11 17:27:00 -0700

    geolocation api set up

--- travel-planner/travel-planner-backend/app.js
+++ travel-planner/travel-planner-backend/app.js
@@ -13,8 +13,39 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(bodyParser.json());
 
+const got = require('got')
+const {pipeline} = require('stream')
+
 app.use("/notes", notes);
 app.use("/users", users);
+
+
+app.post("/getAddress", async (req, res) => {
+
+	// Using this api: https://positionstack.com/
+
+	const API_KEY = "eef710e2f8eeb0f5fbe30931732d2185"
+	const url = `http://api.positionstack.com/v1/forward?access_key=${API_KEY}&query=${req.body.address}&limit=5&output=json`
+	// I can specify a region (this would already be provided if the user wants recommendations)
+
+	// need to connect the user's search bar from the front end here.
+	// need to create a submit button so the user can submit the full address.
+
+	// STRETCH: can we confirm the user typed in a valid address first before doing anything?
+		// Potentially with this api: https://www.smarty.com/products/apis/international-street-api
+
+	const dataStream = got.stream({
+		url: url
+	})
+	pipeline(dataStream, res, (err) => {
+		if (err) {
+			console.log(err)
+			res.sendStatus(500)
+		}
+		res.status(200).send({lat: dataStream.data.latitude, lng: dataStream.data.longitude, address: req.body.address, county: county})
+	})
+
+})
 
 app.get("/", () => {
 	res.status(200).send({ location: "Home page" });
