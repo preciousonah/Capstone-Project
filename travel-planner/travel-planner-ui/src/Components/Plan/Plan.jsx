e6dd27873a77ab194b093fab299b67ec58cba803
Kimberly Truong <kimberlytruong@fb.com>
2022-07-15 13:29:38 -0700

    Added create new map and select map pages

--- travel-planner/travel-planner-ui/src/Components/Plan/Plan.jsx
+++ travel-planner/travel-planner-ui/src/Components/Plan/Plan.jsx
@@ -1,17 +1,41 @@
 import "./Plan.css";
 import Notes from "../Notes/Notes";
 import Maps from "../Maps/Maps";
+import SelectTripPage from "../SelectTrip/SelectTrip";
 
 import { UserContext } from "../../UserContext";
 
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import axios from "axios";
 
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
-const API_KEY = "AIzaSyDUuAbmaWWY2Lk6iKlktVEPRAIrTI0__eg";
+// const API_KEY = "AIzaSyDUuAbmaWWY2Lk6iKlktVEPRAIrTI0__eg";
+
+// fetch all maps and show a drop down.
+// Then the user will select from the dropdown or create a new map
+// When the user creates a new map, they will be prompted for:
+// Title
+// General location (this'll be the center)
+// Then the map will appear, on the left there will be a general timeline on the right
 
 export default function Plan(props) {
+	const [tripDetails, setTripDetails] = useState({ title: "", center: "" });
+
+	useEffect(async () => {
+		// Create a new map (call backend)
+		// Get the geocode for the city
+		// set that as the map's center
+		// set the map title
+
+		axios.post(`http://localhost:${props.PORT}/maps/newMap`, {
+			title: tripDetails.title,
+			center: tripDetails.center
+			});
+
+		console.log(tripDetails);
+	}, [tripDetails]);
+
 	const { sessionToken, signUpUser, logInUser, logOutUser } =
 		useContext(UserContext);
 
@@ -67,28 +91,36 @@ export default function Plan(props) {
 		return <h1>Not working</h1>;
 	};
 
+	// console.log(tripDetails)
+
 	return (
-		<div className="plan-page main-page">
-			<div className="left-app">
-				<Wrapper apiKey={API_KEY} render={render}>
-					<div id="map">
-						{/* insert map here */}
-						{/* Figure out how to add pins to the map here. */}
-						<Maps
-							searchOnChange={updateMapsSearchChange}
-							searchTerm={searchTerm}
-							setCurNote={setCurNote}
-						/>
+		<>
+			{tripDetails.title === "" ? (
+				<SelectTripPage setTripDetails={setTripDetails} />
+			) : (
+				<div className="plan-page main-page">
+					<h1>{tripDetails.title} </h1>
+					<h1>{tripDetails.center}</h1>
+					{/* <div className="left-app">
+						<Wrapper apiKey={API_KEY} render={render}>
+							<div id="map">
+								<Maps
+									searchOnChange={updateMapsSearchChange}
+									searchTerm={searchTerm}
+									setCurNote={setCurNote}
+								/>
+							</div>
+						</Wrapper>
 					</div>
-				</Wrapper>
-			</div>
-			<div className="right-app">
-				<Notes
-					notesText={curNote}
-					updateNoteChange={updateNoteChange}
-					handleSaveOnClick={handleSaveNoteOnClick}
-				/>
-			</div>
-		</div>
+					<div className="right-app">
+						<Notes
+							notesText={curNote}
+							updateNoteChange={updateNoteChange}
+							handleSaveOnClick={handleSaveNoteOnClick}
+						/>
+					</div> */}
+				</div>
+			)}
+		</>
 	);
 }
