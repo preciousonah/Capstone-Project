11310e71059697734f1328bcfc8a76a29ff5f0e0
Kimberly Truong <kimberlytruong@fb.com>
2022-07-18 16:34:08 -0700

    Add routes to create and fetch markers for a map.

--- travel-planner/travel-planner-backend/routes/maps.js
+++ travel-planner/travel-planner-backend/routes/maps.js
@@ -8,17 +8,43 @@ router.use(bodyParser.json());
 const Parse = require("parse/node");
 
 router.post("/newMap", async (req, res) => {
-	let title = req.body.title;
-	let center = req.body.center;
-	let sessionToken = req.body.sessionToken;
-
-	// Pass in center and get lat and lng here:
-	// Use this temporarily since getAddress is broken.
-	const lat = 37.354559;
-	const lng = -121.883844;
-
 	try {
-		mapsModel.createNewMap(title, { lat: lat, lng: lng }, sessionToken);
+		let title = req.body.title;
+		let center = req.body.center;
+		let sessionToken = req.body.sessionToken;
+
+		// Pass in center and get lat and lng here:
+		// Use this temporarily since getAddress is broken.
+		const lat = 30;
+		const lng = 0;
+
+		const Maps = Parse.Object.extend("Maps");
+		const Timelines = Parse.Object.extend("Timelines");
+
+		const map = new Maps();
+
+		let query = new Parse.Query("_Session");
+		query.equalTo("sessionToken", sessionToken);
+		query.first().then(function (session) {
+			if (session) {
+				const user = session.get("user"); // sometimes I get an error... ParseError: User is required. Don't know what's causing this though...
+				map.set("User", user);
+			} else {
+				res.status(400).send({
+					typeStatus: "danger",
+					message: "ERROR: No session found for that token.",
+				});
+			}
+		});
+
+		map.set("MapName", title);
+		map.set("Center", new Parse.GeoPoint({ latitude: lat, longitude: lng }));
+
+		map.set("Timeline", new Timelines());
+
+		map.save().then((newMap) => {
+			res.status(200).send(newMap);
+		});
 	} catch (error) {
 		res.status(400).send({ typeStatus: "danger", message: error });
 	}
@@ -47,9 +73,62 @@ router.post("/getUserMaps", async (req, res) => {
 					console.log("Confused? ", error);
 				});
 		} else {
-			res.status(400).send({ message: "ERROR: Invalid session token!" })
+			res.status(400).send({
+				typeStatus: "danger",
+				message: "ERROR: Invalid session token in getUserMaps!",
+			});
 		}
 	});
+});
+
+router.post("/createNewMarker", async (req, res) => {
+	const mapId = req.body.mapId; // Maps object
+	const location = req.body.location; // {lat: , lng:}
+
+	// or call get address here!
+
+	try {
+		const Markers = Parse.Object.extend("Markers");
+		const Notes = Parse.Object.extend("Notes");
+		const Maps = Parse.Object.extend("Maps");
+
+		const mapPointer = new Maps().set("objectId", mapId);
+
+		const marker = new Markers();
+
+		marker.set("Map", mapPointer);
+		marker.set("Note", new Notes());
+		marker.set(
+			"Location",
+			new Parse.GeoPoint({ latitude: location.lat, longitude: location.lng })
+		);
+
+		marker.save().then((newMarker) => {
+			// return the created marker so it can be added to the map
+			res.status(200).send(newMarker);
+		});
+	} catch (error) {
+		res.status(400).send({ message: error });
+	}
+});
+
+router.post("/getMarkers", async (req, res) => {
+	// get the marker array from the map
+
+	try {
+		const mapId = req.body.mapId;
+
+		const Maps = Parse.Object.extend("Maps");
+		const mapPointer = new Maps().set("objectId", mapId);
+
+		let query = new Parse.Query("Markers");
+		query.equalTo("Map", mapPointer);
+		query.find().then((markers) => {
+			res.status(200).send({ markers: markers });
+		});
+	} catch (error) {
+		res.status(400).send({ message: error });
+	}
 });
 
 router.post("/getAddress", async (req, res) => {
